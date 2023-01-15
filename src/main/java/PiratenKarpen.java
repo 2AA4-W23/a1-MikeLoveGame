ef109c3238e4d1c98ecd7b3b0aab60ca0649765a
Shike Chen <40473396+MikeLoveGame@users.noreply.github.com>
2023-01-14 19:33:37 -0500

    Construct Player class

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -13,31 +13,49 @@ public class PiratenKarpen {
         }
 
         int numGames= Integer.parseInt(args[0]);
-        int count=0;
-        int skullCount=0;
-
-        boolean endgame=false;
 
 
-        while(!endgame && count<numGames){
 
-            for (int i = 0; i <Dices.length; i++) {
-                if(Dices[i]!=null){
-                    faces[i]=Dices[i].roll();
-                }
-                if (faces[i]==Faces.SKULL){
-                    Dices[i]=null;
-                    skullCount++;
-                }
-            }
-            if(skullCount>=3){
-                endgame=true;
-            }
-        }
         System.out.println("That's all folks!");
     }
 
     class Player{
+        public final Dice Dices[]= new Dice[8];
+        public final Faces faces[]= new Faces[Dices.length];
+
+        public String stretagy;
+        public Player(){
+            this("dead roll");
+        }
+        public Player(String Stretagy){
+            stretagy=stretagy;
+        }
+
+        public void round(int numGames){
+            int count=0;
+            int skullCount=0;
+
+            boolean endgame=false;
+
+            while(!endgame && count<numGames){
+
+                for (int i = 0; i <Dices.length; i++) {
+                    if(Dices[i]!=null){
+                        faces[i]=Dices[i].roll();
+                    }
+                    if (faces[i]==Faces.SKULL){
+                        Dices[i]=null;
+                        skullCount++;
+                    }
+                }
+                if(skullCount>=3){
+                    endgame=true;
+                }
+            }
+
+
+        }
+
 
     }
 
